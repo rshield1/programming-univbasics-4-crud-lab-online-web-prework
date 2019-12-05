e6816ca25d6e7d700d00508fab9a88ce3dd8e9da
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 04:24:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  array = ["Rob", "Kenny", "Jared", "Erica"]
   my_array << "Raheem"
 end
 
