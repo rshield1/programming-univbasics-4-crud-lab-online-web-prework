100825819da320db0f7b7295f13cc56fb1ef35d1
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 05:36:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,8 +37,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["Rob", "Kenny", "Jared", "Erica"]
-  array[0]
+ return array[0]
   
 end
 
