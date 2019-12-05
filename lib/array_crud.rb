552d195f178306227d184a6ab16685239ecda6d8
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 04:44:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,31 +13,33 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["Rob", "Kenny", "Jared", "Erica"]
-  my_array.unshift("Alicia")
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ["Rob", "Kenny", "Jared", "Erica"]
-  my_array.pop()
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  my_array = ["Rob", "Kenny", "Jared", "Erica"]
-  my_array.shift()
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array = ["Rob", "Kenny", "Jared", "Erica"]
-  my_array[0]
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  array=[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["Rob", "Kenny", "Jared", "Erica"]
+  array=[-1]
 end
 
 def update_element_from_index(array, index_number, element)
