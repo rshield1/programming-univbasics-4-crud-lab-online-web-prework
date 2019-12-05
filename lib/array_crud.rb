cd240350251b85c564c3a36078f14d952cb655fc
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 05:00:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array = ["Rob", "Kenny", "Jared", "Erica"]
+  array.pop(array)
   array
 end
 
