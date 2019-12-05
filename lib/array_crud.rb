7ebf9213de158e243a61a94650ae145fee69da14
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 05:15:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 def remove_element_from_end_of_array(array)
   array = ["Rob", "Kenny", "Jared", "Erica"]
   last_item = array.pop(-1)
-  
+  last_item
 end
 
 def remove_element_from_start_of_array(array)
