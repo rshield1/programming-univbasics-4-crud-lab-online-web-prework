a80af23d81289e7e16aa98cb515849464697c5d5
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 04:23:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,27 +3,32 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array << "Raheem"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array.unshift("Alicia")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  my_array = ["Rob", "Kenny", "Jared", "Erica"]
+  my_array[0]
 end
 
 def retrieve_first_element_from_array(array)
