00bcf922c3c3971294ba0736a07ee475f433af58
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 05:31:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   array = ["Rob", "Kenny", "Jared", "Erica"]
   array.unshift(element)
-  array
+  return array
 end
 
 def remove_element_from_end_of_array(array)
@@ -32,7 +32,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["Rob", "Kenny", "Jared", "Erica"]
-  my_array[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
